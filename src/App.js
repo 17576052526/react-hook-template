40bef0b6296e8883c91117769b3ec68b4625741d
17576052526 <727863984@qq.com>
2022-05-11 09:16:40 +0800

    -

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Suspense, lazy } from 'react';
+import { HashRouter as Router, Route, Link, NavLink, Redirect } from "react-router-dom";
+
+let Page1 = lazy(() => import('./components/test/page1'))
+let Page2 = lazy(() => import('./components/test/page2'))
+
+//App.js 里面只设置路由，其他不在此处写
+function App() {
+    return (
+        <Suspense fallback="">
+            <Router>
+                {/*<Route path="/" exact component={Page1}></Route>*/}{/*设置主页*/}
+                <Route path="/page1" component={Page1}></Route>
+                <Route path="/page2" component={Page2}></Route>
+            </Router>
+        </Suspense>
+    );
+}
+
+export default App;
