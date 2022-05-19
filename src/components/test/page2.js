aea5660187ec0a985d8096b3b4232fc67af62fef
17576052526 <727863984@qq.com>
2022-05-19 16:12:27 +0800

    -

---
+++ src/components/test/page2.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import axios from 'axios'
+import $ from 'jquery'
+//import common from './../common'
+//import './../plugin'
+//import context from './../../context'
+
+function Test() {
+
+
+
+    return (
+        <div>
+            page2
+        </div>
+    );
+}
+
+export default Test;
