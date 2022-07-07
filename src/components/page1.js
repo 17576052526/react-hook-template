c6da938344db73446fdf78bef0143025dd8d5f78
17576052526 <727863984@qq.com>
2022-07-07 13:55:54 +0800

    -

--- src/components/page1.js
+++ src/components/page1.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext, useRef } from 'react';
 import axios from 'axios'
 import $ from 'jquery'
-import '../plugins/pager'
+import './plugins/pager'
 //import context from './../../context'
 
 export default function () {
